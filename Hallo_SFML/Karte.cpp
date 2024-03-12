7f3d93f77108c064b446a7cae55584bb770eeabf
LouisElt05 <louis.baumgarten.2005@gmail.com>
2024-03-12 15:34:01 +0100

    Skalierung der Spielfläche vergrößert und Geschichte vorerst auskommentiert

--- Hallo_SFML/Karte.cpp
+++ Hallo_SFML/Karte.cpp
@@ -26,7 +26,7 @@ void Karte::erstelleKarte()
         sf::Sprite sprite(texture);
         
         // Skaliere das Sprite um 20%
-        float skalierungsfaktor = 0.8f; // 80% der Originalgröße
+        float skalierungsfaktor = 0.99f; // 80% der Originalgröße
         sprite.setScale(skalierungsfaktor, skalierungsfaktor);
         
         // Berechne die zentrierte Position unter Berücksichtigung der Skalierung
@@ -51,71 +51,73 @@ void Karte::erstelleKarte()
             // Sprite zeichnen
             window.draw(sprite);
 
-/////////////////////////////////////////////////////////
+            //Fenster für Anfangsstory 
+            /*/////////////////////////////////////////////////////////
 
-            // Berechne die Größe des Fensters
-            sf::Vector2u windowSize = window.getSize();
-
-
-            // Erstelle ein Rechteck-Shape
-            sf::RectangleShape box(sf::Vector2f(1100.f, 300.f)); // Größe angepasst
+                        // Berechne die Größe des Fensters
+                        sf::Vector2u windowSize = window.getSize();
 
 
-            // Berechne die Position der Box, damit sie mittig unten im Fenster liegt
-            sf::Vector2f position((windowSize.x - box.getSize().x) / 2.f, windowSize.y - box.getSize().y);
+                        // Erstelle ein Rechteck-Shape
+                        sf::RectangleShape box(sf::Vector2f(1100.f, 300.f)); // Größe angepasst
 
 
-            // Setze die Position des Balkens
-            box.setPosition(position);
-            box.setFillColor(sf::Color(245, 245, 220));
-
-            // Laden der Schriftart
-            sf::Font font;
-            if (!font.loadFromFile("Font/Crimson-Bold.ttf")) {
-                // Fehlerbehandlung, falls die Schriftart nicht geladen werden kann
-                cout << "Fehler" << endl; 
-            }
-
-            // Konfiguration des ersten Textes 
-            sf::Text text;
-            text.setString("Hallo Giovanni! \nIch habe große Neuigkeiten für dich. \nEs ist so weit, mein kleines Restaurant, La Taverna Italiana braucht langsam einen neuen Chef! \nWer könnte es besser machen als du, mein Neffe? Bisher hast du mich noch nie enttäuscht, \negal was ich von dir verlangt habe. \nDu hast Talent, Leidenschaft, und - ganz wichtig - Liebe für Pizza! \n[DRÜCKE SPACE]"); // Textinhalt festlegen
-            text.setCharacterSize(24);
-            text.setFont(font);// Schriftgröße festlegen
-            text.setFillColor(sf::Color::Black); // Farbe festlegen
+                        // Berechne die Position der Box, damit sie mittig unten im Fenster liegt
+                        sf::Vector2f position((windowSize.x - box.getSize().x) / 2.f, windowSize.y - box.getSize().y);
 
 
-            // Position des Textes festlegen (zum Beispiel zentriert in der Box)
-            sf::FloatRect textBounds = text.getLocalBounds();
-            text.setOrigin(textBounds.left + textBounds.width / 2.0f,
-            textBounds.top + textBounds.height / 2.0f);
-            text.setPosition(box.getPosition() + sf::Vector2f(box.getSize().x / 2.0f, box.getSize().y / 2.0f));
+                        // Setze die Position des Balkens
+                        box.setPosition(position);
+                        box.setFillColor(sf::Color(245, 245, 220));
+
+                        // Laden der Schriftart
+                        sf::Font font;
+                        if (!font.loadFromFile("Font/Crimson-Bold.ttf")) {
+                            // Fehlerbehandlung, falls die Schriftart nicht geladen werden kann
+                            cout << "Fehler" << endl; 
+                        }
+
+                        // Konfiguration des ersten Textes 
+                        sf::Text text;
+                        text.setString("Hallo Giovanni! \nIch habe große Neuigkeiten für dich. \nEs ist so weit, mein kleines Restaurant, La Taverna Italiana braucht langsam einen neuen Chef! \nWer könnte es besser machen als du, mein Neffe? Bisher hast du mich noch nie enttäuscht, \negal was ich von dir verlangt habe. \nDu hast Talent, Leidenschaft, und - ganz wichtig - Liebe für Pizza! \n[DRÜCKE SPACE]"); // Textinhalt festlegen
+                        text.setCharacterSize(24);
+                        text.setFont(font);// Schriftgröße festlegen
+                        text.setFillColor(sf::Color::Black); // Farbe festlegen
+
+
+                        // Position des Textes festlegen (zum Beispiel zentriert in der Box)
+                        sf::FloatRect textBounds = text.getLocalBounds();
+                        text.setOrigin(textBounds.left + textBounds.width / 2.0f,
+                        textBounds.top + textBounds.height / 2.0f);
+                        text.setPosition(box.getPosition() + sf::Vector2f(box.getSize().x / 2.0f, box.getSize().y / 2.0f));
             
-            //Abtasten nach der Leertaste 
+                        //Abtasten nach der Leertaste 
             
-            // Behandle Ereignisse
-            sf::Event event2;
-            while (window.pollEvent(event2))
-            {
-                if (event2.type == sf::Event::Closed)
-                {
-                    window.close();
-                }
-            }
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) // Wenn Leertaste gedrückt wird, soll ein neuer text erscheinen 
-            {
-                // Lösche den Text
-                text.setString("In den nächsten Wochen lernst du alles: Teig, Sauce, Belag und ganz wichtig,\n dass Rezepte von deiner Großmutter Assunta Maresca ... also sehe es als Ehre an. \nDie italienische Küche ist mehr als Essen, es ist Tradition, Familie, Leidenschaft! \nDu wirst das großartig während meines Ausflugs in Sizilien machen, da bin ich mir sicher.\nIch bin sicher, du machst La Taverna Italiana noch erfolgreicher, als ich es mir je ausmalen könnte.\nDein Pate \nP.S.: Bitte vergiss nie: Amore ist die wichtigste Zutat!     "); // Textinhalt festlegen
-            }
+                        // Behandle Ereignisse
+                        sf::Event event2;
+                        while (window.pollEvent(event2))
+                        {
+                            if (event2.type == sf::Event::Closed)
+                            {
+                                window.close();
+                            }
+                        }
+                        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) // Wenn Leertaste gedrückt wird, soll ein neuer text erscheinen 
+                        {
+                            // Lösche den Text
+                            text.setString("In den nächsten Wochen lernst du alles: Teig, Sauce, Belag und ganz wichtig,\n dass Rezepte von deiner Großmutter Assunta Maresca ... also sehe es als Ehre an. \nDie italienische Küche ist mehr als Essen, es ist Tradition, Familie, Leidenschaft! \nDu wirst das großartig während meines Ausflugs in Sizilien machen, da bin ich mir sicher.\nIch bin sicher, du machst La Taverna Italiana noch erfolgreicher, als ich es mir je ausmalen könnte.\nDein Pate \nP.S.: Bitte vergiss nie: Amore ist die wichtigste Zutat!     "); // Textinhalt festlegen
+                        }
 
-            // Zeichne die Box auf die Karte
-            window.draw(box);
-
-
-            // Zeichne den Text auf die Karte
-            window.draw(text); 
+                        // Zeichne die Box auf die Karte
+                        window.draw(box);
 
 
-/////////////////////////////////////////////////////////////
+                        // Zeichne den Text auf die Karte
+                        window.draw(text); 
+
+
+            /////////////////////////////////////////////////////////////*/
+
 
             // Fenster anzeigen
            window.display();
@@ -124,21 +126,6 @@ void Karte::erstelleKarte()
     }
 }
 
-/*void Box::erstelleBox() {
 
-    // Berechne die Größe des Fensters
-    sf::Vector2u windowSize = window.getSize();
-
-    // Berechne die Position der Box
-    sf::Vector2f position(0.f, windowSize.y - 50.f);
-
-    // Erstelle ein Rechteck-Shape
-    sf::RectangleShape box(sf::Vector2f(100.f, 50.f));
-    box.setPosition(position);
-    box.setFillColor(sf::Color::White);
-
-    // Zeichne die Box auf die Karte
-    window.draw(box);
-}*/
 
 
